2e92fb6ddecb12e121954db6a3cc06243828f33e
ed <ekenna510@verizon.net>
2019-08-22 20:26:09 -0400

    some changes

--- turtlebot3_bringup/src/turtlebot3_slipcheck.cpp
+++ turtlebot3_bringup/src/turtlebot3_slipcheck.cpp
@@ -14,19 +14,31 @@
 * limitations under the License.
 *******************************************************************************/
 
-/* Authors: Taehoon Lim (Darby) */
+/* Authors: ed kenna
+
+need to check angular velocity to see if it is 0
+
+*/
 
 #include <ros/ros.h>
 #include <sensor_msgs/Imu.h>
 #include <geometry_msgs/Twist.h>
 #include <string>
 #include <std_msgs/Float64.h>
+#include <turtlebot3_msgs/Sound.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
 
-int leftcmd=0, rightcmd=0;
-
+double cmdlinear=0, cmdangular=0;
+//actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> move_base_ac;
+//actionlib_msgs::GoalID cancelgoal;
 bool Moving=false;
 
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+MoveBaseClient ac("move_base", true);
 
+ros::Publisher sound_pub;
+turtlebot3_msgs::Sound sound_;
 //ros::Publisher tb3_version_info_pub;
 //ros::Publisher tb3_diagnostics_pub;
 
@@ -54,8 +66,26 @@ double ox,oy,oz,ow,ax,ay,az,lx,ly,lz;
   lx=msg->linear_acceleration.x;
   ly=msg->linear_acceleration.y;
   lz=msg->linear_acceleration.z;
+  if (cmdlinear != 0.0 || cmdangular != 0.0 )
+  {
+ 
+  if (ax  == 0.0 && ay == 0.0 && az == 0.0)
+  {
   ROS_INFO("imuMsgCallback: %lf %lf %lf %lf %lf %lf %lf %lf %lf",ox,oy,oz,ax,ay,az,lx,ly,lz);
+  ROS_INFO("should be moving but we are not.");
+  sound_pub.publish(sound_);
+  // Cancel all active goals
+  ac.cancelAllGoals(); 
+  }
+  
+  }
+  
+ 
 
+ 
+  
+
+  
   //setIMUDiagnosis(diagnostic_msgs::DiagnosticStatus::OK, "Good Condition");
 }
 void twistMsgCallback(const geometry_msgs::Twist::ConstPtr &msg)
@@ -63,8 +93,16 @@ void twistMsgCallback(const geometry_msgs::Twist::ConstPtr &msg)
   double x,w;
   x=msg->linear.x;
   w=msg->angular.z;
-  ROS_INFO("twistMsgCallback: %lf %lf",x,w);
-
+  if (cmdlinear != x)
+    {
+    cmdlinear = x;
+    ROS_INFO("Updated cmdlinear %lf %lf",x,w );
+    }
+  if (cmdangular != w)
+    {
+    cmdangular = w;
+    ROS_INFO("Updated cmdangular %lf %lf",x,w  );
+    }
 }
 
 
@@ -73,6 +111,9 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "turtlebot3_slipcheck");
   ros::NodeHandle nh;
+
+    sound_pub = nh.advertise<turtlebot3_msgs::Sound>("sound",5,true);
+    sound_.value = 3;
 
   //tb3_diagnostics_pub  = nh.advertise<diagnostic_msgs::DiagnosticArray>("diagnostics", 10);
   //tb3_version_info_pub = nh.advertise<turtlebot3_msgs::VersionInfo>("version_info", 10);
